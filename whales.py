d04785503e9c3eee2926cd5f72794fece7ed1260
Mohammad Hayajneh <35841393+mohammadhayajneh@users.noreply.github.com>
2018-02-17 09:34:25 -0500

    Add files via upload

---
+++ whales.py
@@ -0,0 +1,11 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> whales = [5, 4, 7, 3, 2, 3, 2, 6, 4, 2, 1, 7, 1, 3]
+>>> more_whales=[]
+>>> for i in whales:
+	more_whales.append(i+1)
+
+	
+>>> print (more_whales)
+[6, 5, 8, 4, 3, 4, 3, 7, 5, 3, 2, 8, 2, 4]
+>>> 
